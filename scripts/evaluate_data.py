a96ed016fe208c94802b952f4745bf2ce21578af
Hannah Meyer <vasilisa.kovaliova@gmail.com>
2024-07-01 16:04:23 -0400

    Fixed codepub-copepodTCR inconsistenicies

--- scripts/evaluate_data.py
+++ scripts/evaluate_data.py
@@ -1,4 +1,5 @@
 import copepodTCR as cpp
+import numpy as np
 import pandas as pd
 import random
 
@@ -32,12 +33,13 @@ data = str(args.data)
 output = str(args.output)
 
 cells = pd.read_csv(data, sep = "\t")
-sim_params = pd.read_csv(sim_params, sep = "\t")
+sim_params = pd.read_csv(args.sim_params, sep = "\t")
+n_control = sim_params['n_control'].iloc[0]
 check_results = pd.read_csv(scheme, sep = "\t")
 
 inds = list(cells['Pool'])
 obs = list(cells['Percentage'])
-fig, probs, parameters = cpp.activation_model(obs, args.n_pools, inds, cores = 1)
+fig, probs, parameters = cpp.activation_model(obs, args.n_pools, inds, n_control, cores = 1)
 peptide_probs = cpp.peptide_probabilities(check_results, probs)
 notification, lst1, lst2 = cpp.results_analysis(peptide_probs, probs, check_results)
 cognate = list(check_results['Peptide'][check_results['Cognate'] == True])
@@ -76,6 +78,7 @@ results_row['conclusion_cognate'] = set(cognate) == set(lst1)
 results_row['conclusion_possible'] = all(elem in cognate for elem in lst2)
 results_row['negative_model'] = parameters[1]
 results_row['positive_model'] = parameters[0]
+results_row['neg_control'] = sim_params['n_control'].iloc[0]
 results_row['positive_sim'] = sim_params['positive_sim'].iloc[0]
 results_row['negative_sim'] = sim_params['negative_sim'].iloc[0]
 
--- scripts/sim_data.py
+++ scripts/sim_data.py
@@ -46,7 +46,7 @@ n_shape = args.n_pools - p_shape - pl_shape
 
 inds_n_check = [item for item in range(args.n_pools) if item not in inds_p_check and item not in inds_pl_check]
 
-p_results, pl_results, n_results, parameters = simulation(args.mu_off, args.sigma_off, args.mu_n, args.sigma_n, args.r,
+p_results, pl_results, n_results, n_control, parameters = cpp.simulation(args.mu_off, args.sigma_off, args.mu_n, args.sigma_n, args.r,
     args.sigma_p_r, args.sigma_n_r, args.n_pools, p_shape, pl_shape, args.low_offset)
 
 cells = pd.DataFrame({
@@ -54,7 +54,7 @@ cells = pd.DataFrame({
     'Percentage': p_results + pl_results + n_results
 })
 
-sim_parameters = pd.DataFrame({'negative_sim': parameters[1], 'positive_sim': parameters[0]})
+sim_parameters = pd.DataFrame({'n_control':n_control[0], 'negative_sim': parameters[1], 'positive_sim': parameters[0]}, index = [0])
 
 # Writing output
 cells.to_csv(args.output, sep="\t", index=None)
